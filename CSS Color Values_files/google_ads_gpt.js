7487390d24b282e3970b73ade93a2c449a86b7a5
ehsan abdolahi <ehsanab96@gmail.com>
2012-09-24 16:49:23 +0330

    upd

--- CSS Color Values_files/google_ads_gpt.js
+++
@@ -1,2 +0,0 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1256"><title>M5-3
-</title></head><body><iframe src="http://10.10.34.34?type=Invalid Site&policy=MainPolicy " style="width: 100%; height: 100%" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0"></iframe></body></html>
